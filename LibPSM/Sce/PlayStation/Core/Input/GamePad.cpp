4c7481ec274f435948d207462441d19afcf20937
Bluzume <39113159+KuromeSan@users.noreply.github.com>
2022-01-30 00:35:09 -0500

    Update GamePad.hpp

--- LibPSM/Sce/PlayStation/Core/Input/GamePad.cpp
+++ LibPSM/Sce/PlayStation/Core/Input/GamePad.cpp
@@ -1,7 +1,7 @@
 #include "GamePad.hpp"
 
 namespace Sce::PlayStation::Core::Input {
-	int GamePad::GetDataNative(int, Sce::PlayStation::Core::Input::GamePadData *){
+	int GamePad::GetDataNative(int deviceIndex, GamePadData* gamePadData) {
 		cout << "Sce::PlayStation::Core::Input::GetDataNative(int, Sce::PlayStation::Core::Input::GamePadData *) Unimplemented." << endl;
 		return 0;
 	}
--- LibPSM/Sce/PlayStation/Core/Input/GamePad.hpp
+++ LibPSM/Sce/PlayStation/Core/Input/GamePad.hpp
@@ -4,12 +4,13 @@
 #include <stdlib.h>
 #include <string>
 #include <iostream>
+#include "GamePadData.hpp"
 using namespace std;
 
 namespace Sce::PlayStation::Core::Input {
 	class GamePad {
 	public:
-		static int GetDataNative(int, Sce::PlayStation::Core::Input::GamePadData *);
+		static int GetDataNative(int deviceIndex, GamePadData *gamePadData);
 	};
 }
 #endif
